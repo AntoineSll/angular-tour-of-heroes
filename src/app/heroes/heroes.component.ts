aa2b799c9320810317e0af56b1165e0eea991ef9
AntoineSll <antoine.salle77@gmail.com>
2022-08-18 16:56:05 +0200

    Button event + display details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 
@@ -10,6 +11,11 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
   
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
   constructor() { }
 
   ngOnInit(): void {
