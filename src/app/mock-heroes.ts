58d0d609b83b6578063baf2d00d0d13fea00a345
AntoineSll <antoine.salle77@gmail.com>
2022-08-19 15:16:04 +0200

    Button fix

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    { id: 12, name: 'Dr. Orel' },
-    { id: 13, name: 'QuentinMalin' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magnetanto' },
-    { id: 16, name: 'RubberVince' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr. IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 1, name: 'Dr. Orel' },
+    { id: 2, name: 'QuentinMalin' },
+    { id: 3, name: 'Celeritas' },
+    { id: 4, name: 'Magnetanto' },
+    { id: 5, name: 'RubberVince' },
+    { id: 6, name: 'Dynama' },
+    { id: 7, name: 'Dr. IQ' },
+    { id: 8, name: 'Magma' },
+    { id: 9, name: 'Tornado' }
   ];
